4e4dcb3ff07586642f812842d40308344e6672da
Mason Dykstra <masondykstra01@gmail.com>
2023-10-05 10:23:45 -0400

    finished roman numerals

--- roman-numerals/romanNumerals.js
+++ roman-numerals/romanNumerals.js
@@ -1,5 +1,5 @@
 // create an object of key value pairs to represent roman numerals
-const romanNumeralToArabic = {
+const romanNumeralToArabicLazy = {
   'I': 1,
   'V': 5,
   'X': 10,
@@ -9,28 +9,56 @@ const romanNumeralToArabic = {
   'M': 1000,
 }
 
-const romanNumeralPriority = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+const romanNumeralPriorityLazy = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
 
 function toRomanLazy(num) {
   let output = ""
   
-  for (key of romanNumeralPriority) {
-    console.log(Math.floor(num/romanNumeralToArabic[key]))
+  for (key of romanNumeralPriorityLazy) {
+    let multiple = Math.floor(num/romanNumeralToArabicLazy[key])
+    for (i = multiple; i > 0; i--) {
+      output += key
+      num -= romanNumeralToArabicLazy[key]
+    }
     // Math.floor(num/romanNumeralToArabic[key])
-    // suntract that amount from num
+    // subtract that amount from num
   }
+  return output
 }
 
-toRomanLazy(23)
 
 
 
+const romanNumeralToArabic = {
+  'I': 1,
+  'IV': 4,
+  'V': 5,
+  'IX': 9,
+  'X': 10,
+  'XL': 40,
+  'L': 50,
+  'C': 100,
+  'CD': 400,
+  'D': 500,
+  'CM': 900,
+  'M': 1000,
+}
 
-
+const romanNumeralPriority = ['M', 'CM', 'D', 'CD', 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
 
 function toRoman(num) {
-  return "";
+  let output = ""
+  
+  for (key of romanNumeralPriority) {
+    let multiple = Math.floor(num/romanNumeralToArabic[key])
+
+    for (i = multiple; i > 0; i--) {
+      output += key
+      num -= romanNumeralToArabic[key]
+    }
+  }
+  return output
 }
 
 module.exports = { toRoman, toRomanLazy };
