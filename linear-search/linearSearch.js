8f75e8dba86063401c56b94cf9a30d1584af3de6
Mason Dykstra <masondykstra01@gmail.com>
2023-10-07 11:11:28 -0400

    idk

--- linear-search/linearSearch.js
+++ linear-search/linearSearch.js
@@ -1,19 +1,21 @@
-function linearSearch(searchTerm, arr) {
-  // create a loop to iterate through the array
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === searchTerm) {
-      return i
-    }
-    else{
-      return undefined;
-    }
-  }
-}
+// function linearSearch(searchTerm, arr) {
+//   // create a loop to iterate through the array
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === searchTerm) {
+//       return i
+//     }
+//     else{
+//       return undefined;
+//     }
+//   }
+// }
 
-console.log(linearSearch(3, [1, 2, 3]))
+// console.log(linearSearch(3, [1, 2, 3]))
 
-function globalLinearSearch(searchTerm, arr) {
-  return [];
-}
+// function globalLinearSearch(searchTerm, arr) {
+//   return [];
+// }
 
-module.exports = { linearSearch, globalLinearSearch };
+// module.exports = { linearSearch, globalLinearSearch };
+
+let input = prompt('enter something: ')
