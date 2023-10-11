7801721904c7faa638f9e28828ac13d58b264761
Mason Dykstra <masondykstra01@gmail.com>
2023-10-11 15:48:41 -0400

    user app

---
+++ app-users-ii/User.py
@@ -0,0 +1,36 @@
+class User:
+    post = []
+    
+    def __init__(self, name, email, dlc_num, username):
+        self.name = name
+        self.email = email
+        self.dlc_num = dlc_num
+        self.username = username
+        self.user_posts = []
+    
+    def __str__(self):
+        return f"Name: {self.name}\nEmail: {self.email}\nUsername: {self.username}\nDriver's License Number: {self.dlc_num}"
+    
+    def make_post(self):
+        new_post = input('What would you like to post?: ')
+        self.user_posts.append(new_post)
+        User.post.append(new_post)
+
+    @staticmethod
+    def view_all_posts():
+        for content in User.post:
+            print(content)
+    
+    def view_user_posts(self):
+        for content in self.user_posts:
+            print(content)
+
+# mason = User('Mason', 'mason@stuff.org', 23423423, 'masonlee06')
+# regis = User('Regis', 'regis@stuff.com', 1423452, 'regi')
+
+# mason.make_post()
+# mason.make_post()
+# regis.make_post()
+# mason.view_user_posts()
+# regis.view_user_posts()
+# User.view_all_posts()
--- linear-search/linearSearch.js
+++ linear-search/linearSearch.js
@@ -1,21 +1,19 @@
-// function linearSearch(searchTerm, arr) {
-//   // create a loop to iterate through the array
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === searchTerm) {
-//       return i
-//     }
-//     else{
-//       return undefined;
-//     }
-//   }
-// }
+function linearSearch(searchTerm, arr) {
+  // create a loop to iterate through the array
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === searchTerm) {
+      return i
+    }
+    else{
+      return undefined;
+    }
+  }
+}
 
-// console.log(linearSearch(3, [1, 2, 3]))
+console.log(linearSearch(3, [1, 2, 3]))
 
-// function globalLinearSearch(searchTerm, arr) {
-//   return [];
-// }
+function globalLinearSearch(searchTerm, arr) {
+  return [];
+}
 
-// module.exports = { linearSearch, globalLinearSearch };
-
-let input = prompt('enter something: ')
+module.exports = { linearSearch, globalLinearSearch };
