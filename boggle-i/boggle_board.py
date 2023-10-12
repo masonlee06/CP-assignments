e413af134ce13d30d83847b7ae2a1a96451dcf44
Mason Dykstra <masondykstra01@gmail.com>
2023-10-12 16:53:16 -0400

    it works

--- boggle-i/boggle_board.py
+++ boggle-i/boggle_board.py
@@ -1,6 +1,51 @@
+import random
+
+
+
 class BoggleBoard:
   
+  dice = [
+        "AQBEGN",
+        "ELRTKY",
+        "AOVTQW",
+        "ABYJOC",
+        '',
+        "EHRTVW",
+        "CIMOTU",
+        "DISTTY",
+        "EIOSST",
+        '',
+        "DELRVY",
+        "OUWXAP",
+        "HIMNQU",
+        "EEINSU",
+        ',',
+        "EEGHNW",
+        "AFFKPS",
+        "HLNNRZ",
+        "QGEXSH"
+    ]
+
   def __init__(self):
+    self.board = ['_', '_', '_', '_', '\n', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '\n', '_', '_', '_', '_']
+  
+  def __str__(self):
+    return ''.join(self.board)
   
 
+
   def shake(self):
+    i=0
+    while i < 19:
+      if self.board[i] == '_' or self.board[i].isalpha():
+        self.board[i] = random.choice(BoggleBoard.dice[i])
+        self.board[i] += '  '
+        if self.board[i] == 'Q  ':
+          self.board[i] = 'Qu '
+      i+=1
+
+# board = BoggleBoard()
+
+# board.shake()
+
+# print(board)
--- boggle-ii/boggle_board.py
+++ boggle-ii/boggle_board.py
@@ -1,11 +1,86 @@
 # You should re-use and modify your old BoggleBoard class to support the new requirements
-
+import random
 class BoggleBoard:
-	def __init__(self):
-      pass
+        
 
-	def shake(self):
-      pass
-  
-  def include_word(self):
-      pass
+            dice = [
+            "SEGXAL",
+            "CIMOTU",
+            "DISTTY",
+            "EIOSST",
+            '',
+            "AQBEGN",
+            "IGBWPX",
+            "AOVTQW",
+            "ABYJOC",
+            '',
+            "DELRVY",
+            "OUWXAP",
+            "URENOW",
+            "EEINSU",
+            ',',
+            "EEGHNW",
+            "AFFKPS",
+            "HLNNRZ",
+            "YGNMQW"
+            ]
+
+            def __init__(self):
+                  self.board = ['_', '_', '_', '_', '\n', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '\n', '_', '_', '_', '_']
+                  self.row1 = []
+                  self.row2 = []
+                  self.row3 = []
+                  self.row4 = []
+                  
+      
+            def __str__(self):
+                  return ''.join(self.board)
+                 
+      
+
+
+            def shake(self):
+                  i=0
+                  while i < 19:
+                        if self.board[i] == '_' or self.board[i].isalpha():
+                              self.board[i] = random.choice(BoggleBoard.dice[i])
+                              self.board[i] += '  '
+                        if self.board[i] == 'Q  ':
+                              self.board[i] = 'Qu '
+                        i+=1
+                  self.row1.append(self.board[0: 4])
+                  self.row2.append(self.board[5: 9])
+                  self.row3.append(self.board[10: 14])
+                  self.row4.append(self.board[15: 20])
+
+
+            def include_word(self, word):
+                  row1 = ''.join(self.row1[0]).replace(' ', '')
+                  row2 = ''.join(self.row2[0]).replace(' ', '')
+                  row3 = ''.join(self.row3[0]).replace(' ', '')
+                  row4 = ''.join(self.row4[0]).replace(' ', '')
+                  col1 = f"{row1[0]}{row2[0]}{row3[0]}{row4[0]}"
+                  col2 = f"{row1[1]}{row2[1]}{row3[1]}{row4[1]}"
+                  col3 = f"{row1[2]}{row2[2]}{row3[2]}{row4[2]}"
+                  col4 = f"{row1[3]}{row2[3]}{row3[3]}{row4[3]}"
+                  diag1 = f"{row1[0]}{row2[1]}{row3[2]}{row4[3]}"
+                  diag2 = f"{row4[0]}{row3[1]}{row2[2]}{row1[3]}"
+                  all_words = [row1, row2, row3, row4, col1, col2, col3, col4, diag1, diag2]
+                  user_word = word.upper()
+                  rev_word = user_word[::-1]
+                  for item in all_words:
+                        if user_word == item or rev_word == item:
+                                return True
+                  else:
+                        return False
+                 
+
+
+board = BoggleBoard()
+
+board.shake()
+print(board)
+print(board.row1)
+print(board.include_word('aaaa'))
+
+
