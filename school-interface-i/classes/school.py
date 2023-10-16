13019f1ec479112da35fbcb12cebed234210aa4e
Mason Dykstra <masondykstra01@gmail.com>
2023-10-16 09:46:18 -0400

    school

--- school-interface-i/classes/school.py
+++ school-interface-i/classes/school.py
@@ -7,3 +7,13 @@ class School:
         self.name = name
         self.staff = Staff.all_staff()
         self.students = Student.all_students()
+
+    def __str__(self):
+        return f"""{self.name}
+---------------
+age: {self.age}
+id: {self.student_id}"""
+
+    def list_students(self):
+        for idx, stud in self.students:
+            print(f'{idx + 1}. {stud.name} {stud.school_id}')
--- school-interface-i/runner.py
+++ school-interface-i/runner.py
@@ -2,6 +2,17 @@ from classes.school import School
 
 school = School('Ridgemont High') 
 
-print(school.name)
-print(school.students)
-print(school.staff)
+# print(school.name)
+# print(school.students)
+# print(school.staff)
+
+
+while True:
+    choice = input(print("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\nEnter:"))
+    if choice == '5':
+        print('Goodbye!')
+        break
+    elif choice == '2':
+        student_id = input('Enter student id: ')
+    if choice == '1':
+        school.list_students()
